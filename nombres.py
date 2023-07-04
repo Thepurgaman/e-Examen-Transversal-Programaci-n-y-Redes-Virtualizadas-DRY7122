6365248c93c411060fb2e93eea2b2b42f8f507ab
Thepurgaman <129972344+Thepurgaman@users.noreply.github.com>
2023-07-04 18:05:54 -0400

    nombres.py examen
    Script del  Examen Transversal

---
+++ nombres.py
@@ -0,0 +1,8 @@
+# Script para imprimir una lista de nombres
+
+nombres = ['Andres Avalos', 'Angel Contreras', 'Francisco Fernandez', 'alex Sanhueza']
+
+# Imprimir los nombres en la lista
+for nombre in nombres:
+    print(nombre)
+
